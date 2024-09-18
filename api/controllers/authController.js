79850d4f6e9871aa73b18be13761a671c8c7a4c7
Rehana <nfsrehana@gmail.com>
2024-09-18 17:32:59 +0530

    complete signup page Functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,15 +1,15 @@
 import User from "../models/userModel.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10)
-  const newUser = new User({ username, email, password:hashedPassword });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json({ message: "User Created Successfully " });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
 
