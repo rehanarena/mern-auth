eb7efde13d4758f2a34f8230597324a363575c38
Rehana <nfsrehana@gmail.com>
2024-09-18 12:37:26 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.js";
+import authRoutes from "./routes/auth.js";
 dotenv.config();
 
 mongoose
@@ -12,7 +14,11 @@ mongoose
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth",authRoutes)
