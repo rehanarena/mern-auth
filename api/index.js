7374e2e9a196e890671176c4d9b423dbe2eb3878
Rehana <nfsrehana@gmail.com>
2024-09-20 01:28:22 +0530

    add pre production Changes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -14,7 +15,15 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const __dirname = path.resolve();
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -23,15 +32,14 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRoutes);
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
 
-
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal  Server Error';
+  const message = err.message || "Internal  Server Error";
   return res.status(statusCode).json({
     success: false,
     message,
     statusCode,
-  })
-})
+  });
+});
